83c9a97b2dddf2474c35ee53eb42cc2ac5c8321f
jdado <jakub.dado@gmail.com>
2015-11-29 23:42:09 +0100

    initial2

--- record.js
+++ record.js
@@ -3,6 +3,32 @@ var useSendBeacon = true;
 var useExternalServerScript = true;
 var externalServerScriptUrl = "http://147.175.149.192:443/logger";
 var visitorProperty = "";
+var apiKey = ""
+
+
+function apiKeyFromScript(){
+	var scripts = document.getElementsByTagName('script');
+	var myScript = scripts[ scripts.length - 1 ];
+
+	var queryString = myScript.src.replace(/^[^\?]+\??/,'');
+
+	var params = parseQuery( queryString );
+}
+
+function parseQuery ( query ) {
+  var Params = new Object ();
+  if ( ! query ) return Params; // return empty object
+  var Pairs = query.split(/[;&]/);
+  for ( var i = 0; i < Pairs.length; i++ ) {
+    var KeyVal = Pairs[i].split('=');
+    if ( ! KeyVal || KeyVal.length != 2 ) continue;
+    var key = unescape( KeyVal[0] );
+    var val = unescape( KeyVal[1] );
+    val = val.replace(/\+/g, ' ');
+    Params[key] = val;
+  }
+  return Params;
+}
 
 function guid() {
     function _p8(s) {
